72933c5221c82bf9f159f495c4e9f26dcb80cd3a
Manlio García <quarkex@gmail.com>
2018-04-25 13:51:20 +0100

    added class to the main content

--- js/directives/gui.js
+++ js/directives/gui.js
@@ -4,7 +4,7 @@ app.directive('appGui', function () {
         '<app-header class="hide-for-print"></app-header>' +
         '<app-titlebar class="hide-for-print"></app-titlebar>' +
         '<app-sliding-banner class="hide-for-print"></app-sliding-banner>' +
-        '<div ng-transclude flex layout="column"></div>';
+        '<div ng-transclude flex layout="column" class="main"></div>';
     template = '<div layout="column" flex flex-gt-xs="90">' + template + '</div>';
     template = '<div layout="row" flex="100" layout-align="center start" style="min-height: 100vh;">' + template + '</div>';
 
