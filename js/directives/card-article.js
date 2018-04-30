1067a0c4c6ef8543b752db3f3a0dc2899bff36d6
Manlio García <quarkex@gmail.com>
2018-04-30 14:20:49 +0100

    added margin to article cards

--- js/directives/card-article.js
+++ js/directives/card-article.js
@@ -1,10 +1,10 @@
 app.directive('appCardArticle', function () {
 
-    var template = '<article layout="row" flex>' +
-        '<div flex="33">' +
+    var template = '<article layout-gt-xs="row" layout="column" flex>' +
+        '<div flex="100" flex-gt-xs="33">' +
             '<figure bind-html-compile="item.figure"></figure>' +
         '</div>' +
-        '<div flex="66" layout="column" style="margin-right: 3em;">' +
+        '<div flex="100" flex-gt-xs="66" layout="column">' +
             '<header>' +
                 '<h4><a href="{{ prefix }}/{{ item.id }}">{{ item.title }}</a></h4>' +
             '</header>' +
@@ -13,6 +13,8 @@ app.directive('appCardArticle', function () {
             '<footer>' +
                 '<a href="{{ prefix }}/{{ item.id }}">Leer más</a><br>' +
             '</footer>' +
+        '</div>' +
+        '<div hide-xs show-gt-xs style="margin-right: 2.5em;">' +
         '</div>' +
     '</article>';
 
