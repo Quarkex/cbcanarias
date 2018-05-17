7d657ef9666d5409b5240d15bad9a159e47dc90a
Manlio García <quarkex@gmail.com>
2018-05-17 02:41:29 +0100

    replaced ads references

--- js/directives/ads-banner.js
+++ js/directives/ads-banner.js
@@ -1,7 +1,11 @@
 app.directive('appAdsBanner', function () {
 
-    var template = '<div flex="100" layout="column" layout-align="center center">' +
-        '<img src="/img/ads/Imberostar-H.gif" alt="Imberostar">' +
+    var template = '' +
+    '<div flex flex-gt-xs="50" layout="column" layout-align="center center">' +
+        '<a href="#"><img src="/img/ads/01.gif"></a>' +
+    '</div>' +
+    '<div flex flex-gt-xs="50" layout="column" layout-align="center center">' +
+        '<a href="#"><img src="/img/ads/02.gif"></a>' +
     '</div>';
 
     return {
