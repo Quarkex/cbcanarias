e655952e5d28367b02a240a40307237c3849f2bb
Manlio García <quarkex@gmail.com>
2018-06-20 14:40:00 +0100

    added pusher in social media

--- js/directives/social-media.js
+++ js/directives/social-media.js
@@ -2,11 +2,12 @@ app.directive('appSocialMedia', function () {
 
     var template = '' + //'<h2>Redes</h2>' +
     '<div flex="100" layout="column" layout-gt-md="row" layout-wrap>' +
-        '<div flex="100" flex-gt-md="66">' +
+        '<div flex="100" flex-gt-md="65">' +
             '<div class="flex-video"><iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=Canarias1939" width="480" height="400"></iframe></div>' +
         '</div>' +
-        '<div flex="100" flex-gt-md="33">' +
-            '<a class="twitter-timeline" data-height="565" href="https://twitter.com/CB1939Canarias?ref_src=twsrc%5Etfw">Tweets by CB1939Canarias</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>' +
+        '<div flex="100" flex-gt-md="5"></div>' +
+        '<div flex="100" flex-gt-md="30">' +
+            '<a class="twitter-timeline" data-height="560" href="https://twitter.com/CB1939Canarias?ref_src=twsrc%5Etfw">Tweets by CB1939Canarias</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>' +
         '</div>' +
     '</div>';
 
